9c7c0945a041dcb0ab1bf0ebd6ba35195121c98f
KhunHon <thehonkungleve0522@gmail.com>
2022-10-25 17:49:26 +0700

    first commit

---
+++ src/main/kotlin/02_Operation.kt
@@ -0,0 +1,27 @@
+fun kMessage(){
+    println("=================")
+}
+
+
+fun kDataType(){
+    println("Signed Integral Type")
+    val kByte:UByte = 255u
+    val kShort:UShort = 65_535u
+    println("val kByte = $kByte")
+    println("val UShort = $kShort")
+
+}
+
+fun kOperator(){
+    val a:Byte = 4 + 2
+    val b:Byte = 4-2
+    val c:Byte = 4*2
+    val d:Byte = 4/2
+    var e:Byte = 4%2
+    println("2.Operator in java")
+    println("val a = $a")
+    println("val b = $b")
+    println("val b = $c")
+    println("val b = $d")
+    println("var b = $e")
+}
